1898cfdb87edaf249f99c6418240c30e0f6ef11a
Kindlebit <devopskindlebit@gmail.com>
2024-01-29 17:03:36 +0530

    new code

---
+++ practice.py
@@ -0,0 +1,28 @@
+print("hello world")
+name= "aditya"
+age= 20
+print(name,age)
+x=10
+y=20
+z=(x+y)
+print(z)
+print(x<y)
+print(x>y)
+print(y>x)
+
+a= "ram"
+b="charan"
+z=(a+" "+b)
+print(z)
+x, y, z = "mango", "orangle", "apple"
+print(x)
+x=y=z = "tea"
+print(x)
+print(z)
+
+name = ["roshan", "rohit", "mohan"]
+print(name)
+x, y, z = name
+d=(name)
+print(d)
+print(x)
